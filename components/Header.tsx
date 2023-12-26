8d17c1fac4d6be52a3f2f6883d68f9d2af05bf90
Alex <alex@jintronix.com>
2023-12-25 20:14:17 -0500

    discord link for feedback

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,15 +7,21 @@ export class Header extends Component {
       <div className="flex flex-col gap-2">
         <div className="flex justify-between items-center">
           <div className="flex items-end gap-4">
-            <h1 className="text-6xl font-bold text-center text-teal-500">
+            <span className="text-6xl font-bold text-center text-teal-500">
               Not Even Close
-            </h1>
-            <h1 className="font-bold text-center text-teal-500">
-              {"by Ortemist-Zul'jin"}
-            </h1>
+            </span>
+            <span className="font-bold text-center text-teal-500">
+              by Ortemist-Zul'jin
+            </span>
           </div>
           <GitHubButton href="https://github.com/acornellier/not_even_close" />
         </div>
+        <span className="font-bold text-teal-500">
+          Disclaimer: Very WIP! Expect errors and bugs.{' '}
+          <a className="text-blue-500" href="https://discord.gg/Ykb6AbYHHZ">
+            Click here to join my Discord community for feedback
+          </a>
+        </span>
       </div>
     )
   }
