54b0ea3cb970c9feade3f93807ece51414f37b7e
Alex <alex@jintronix.com>
2024-01-03 16:00:48 -0500

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   )
 }
