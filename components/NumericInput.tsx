282269874a9c9385b743c50c3773c37606cb99ff
Alex <alex@jintronix.com>
2024-01-06 00:14:23 -0500

    minor styling change

--- components/NumericInput.tsx
+++ components/NumericInput.tsx
@@ -13,7 +13,7 @@ export function NumericInput({ label, max, min, step = 1, onChange, value }: Pro
       <label className="block font-bold mb-1 pr-4">{label}</label>
       <div>
         <input
-          className="bg-gray-200 dark:bg-zinc-700 border-2 border-gray-200 dark:border-zinc-600 text-black dark:text-white rounded w-40 py-1.5 px-4 focus:outline-none focus:bg-white dark:focus:bg-zinc-800 focus:border-teal-500 dark:focus:border-teal-500"
+          className="bg-gray-200 dark:bg-zinc-700 border-2 border-gray-200 dark:border-zinc-600 text-black dark:text-white rounded w-44 py-1.5 px-4 focus:outline-none focus:bg-white dark:focus:bg-zinc-800 focus:border-teal-500 dark:focus:border-teal-500"
           type="number"
           min={min}
           max={max}
