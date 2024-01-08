6a71c53752841fff811cca04645bbf72ab77835b
Alex Cornellier <alex@jintronix.com>
2024-01-08 14:16:06 -0500

    add a tip about not double counting stats

--- components/Instructions.tsx
+++ components/Instructions.tsx
@@ -3,6 +3,11 @@ export function Instructions() {
     <div className="flex flex-col gap-2">
       <span className="font-bold">Instructions</span>
       <span>
+        Input your stats without any buffs that could affect those stats. Be careful not
+        to double count Fortitude and Mark of the Wild. Do include your vers phial though,
+        as this tool cannot account for it.
+      </span>
+      <span>
         Click a boss ability in the bottom left to set the base damage. If your ability is
         missing, find the base damage in the dungeon journal.
       </span>
