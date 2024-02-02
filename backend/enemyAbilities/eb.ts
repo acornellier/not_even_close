9b57a0479cd94fc89101be38f245358a94d4e079
Alex Cornellier <alex@jintronix.com>
2024-02-02 12:52:39 -0500

    add Agitated Water

--- backend/enemyAbilities/eb.ts
+++ backend/enemyAbilities/eb.ts
@@ -1,5 +1,14 @@
 import { EnemyAbility } from '../dungeons'
 
+export const agitatedWater: EnemyAbility = {
+  name: 'Agitated Water',
+  dungeon: 'Everbloom',
+  baseDamage: 82396,
+  isAoe: true,
+  iconName: 'spell_frost_frostbolt',
+  wowheadLink: 'https://www.wowhead.com/spell=177734/agitated-water',
+}
+
 export const colossalBlow: EnemyAbility = {
   name: 'Colossal Blow',
   boss: 'Yalnu',
@@ -10,4 +19,4 @@ export const colossalBlow: EnemyAbility = {
   wowheadLink: 'https://www.wowhead.com/spell=169179/colossal-blow',
 }
 
-export const ebAbilities = [colossalBlow]
+export const ebAbilities = [agitatedWater, colossalBlow]
