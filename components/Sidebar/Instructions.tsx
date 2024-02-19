3bde6f5cd7cb1ed994d49a7d895abce63c4c7240
Alex <alex@jintronix.com>
2024-02-19 10:31:48 -0500

    update instructions

--- components/Sidebar/Instructions.tsx
+++ components/Sidebar/Instructions.tsx
@@ -18,8 +18,7 @@ export function Instructions() {
         </a>{' '}
         to quickly output stats. Import them by clicking the paste icon to the right of
         the character stats (not on Firefox), or by pressing ctrl-V/cmd-V if you only have
-        1 character. It accounts for vers phial, Fortitude and MotW, but not other
-        stamina/vers buffs.
+        1 character. It accounts for vers phial, Fortitude and Mark of the Wild.
       </span>
       <span>
         If you have multiple characters or builds, create profiles for them with the +
@@ -31,7 +30,7 @@ export function Instructions() {
         Charge are not reduced by armor.
       </span>
       <span>
-        To give feedback or see the changelog,{' '}
+        To give feedback or view the changelog,{' '}
         <a
           className="text-blue-600 dark:text-blue-500 hover:underline"
           href="https://discord.com/invite/Ykb6AbYHHZ"
