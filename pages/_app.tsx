7b5df0b804f5dda8981cd92fcc42c23907255b22
Alex <alex@jintronix.com>
2024-03-27 23:15:56 -0400

    fix leftover light mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'next-themes'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider attribute="class" defaultTheme="dark">
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
+  return <Component {...pageProps} />
 }
