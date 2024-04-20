73f27791f8a122066129dbf347f08ae3d409569c
Alex <alex@jintronix.com>
2024-04-20 09:18:24 -0400

    debug

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import TagManager from 'react-gtm-module'
 import { App } from './App'
 // import { StrictMode } from 'react'
 
-// if (process.env.NODE_ENV === 'production')
-console.log('hi')
 TagManager.initialize({ gtmId: 'G-N6CRB9B3CL' })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
