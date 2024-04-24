4b0b596ced5e3aecb4f11049111a387892e61856
Alex <alex@jintronix.com>
2024-04-24 08:56:28 -0400

    update grimoire

--- src/backend/enemyAbilities/grimoireConverter.ts
+++ src/backend/enemyAbilities/grimoireConverter.ts
@@ -6,7 +6,7 @@ export function grimoireToEnemyAbility(spell: GrimoireSpell): EnemyAbility {
     id: spell.id,
     name: spell.name,
     icon: spell.icon,
-    damage: spell.damage?.s4 ?? 0,
+    damage: spell.damage ?? 0,
     aoe: !!spell.aoe,
     physical: !!spell.physical,
     variance: (spell.variance ?? 0) / 2,
