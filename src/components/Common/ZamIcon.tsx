0034911680388fe8a652ff46d0de33dd3ccf1b4d
Alex <alex@jintronix.com>
2024-04-29 21:04:08 -0400

    don't use vercel edge caching for icons

--- src/components/Common/ZamIcon.tsx
+++ src/components/Common/ZamIcon.tsx
@@ -1,6 +1,5 @@
-import type { DetailedHTMLProps, ImgHTMLAttributes } from 'react';
+import type { DetailedHTMLProps, ImgHTMLAttributes } from 'react'
 import { forwardRef } from 'react'
-import { isDev } from '../../util/env.ts'
 
 interface Props
   extends DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> {
@@ -16,11 +15,7 @@ export const ZamIcon = forwardRef<HTMLImageElement, Props>((props, ref) => {
       alt={props.alt}
       width={props.size}
       height={props.size}
-      src={
-        isDev
-          ? props.src
-          : `/_vercel/image?url=${encodeURIComponent(props.src)}&w=56&q=100`
-      }
+      src={props.src}
     />
   )
 })
