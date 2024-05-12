f88ac6ae3b105993fa1416107fd599bdaa246abb
Alex <alex@jintronix.com>
2024-05-12 11:20:59 -0400

    hide absorbs in results if there are none

--- src/components/Sidebar/ResultDetails.tsx
+++ src/components/Sidebar/ResultDetails.tsx
@@ -22,8 +22,12 @@ export function ResultDetails({ charResult }: Props) {
         Actual damage taken: {charResult.actualDamageTaken.toLocaleString('en-US')}
       </div>
       <div>Starting health: {charResult.startingHealth.toLocaleString('en-US')}</div>
-      <div>Absorbs: {charResult.absorbs.toLocaleString('en-US')}</div>
-      <div>Total health: {charResult.totalHealth.toLocaleString('en-US')}</div>
+      {charResult.absorbs > 0 && (
+        <>
+          <div>Absorbs: {charResult.absorbs.toLocaleString('en-US')}</div>
+          <div>Total health: {charResult.totalHealth.toLocaleString('en-US')}</div>
+        </>
+      )}
     </>
   )
 }
