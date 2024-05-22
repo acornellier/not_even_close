9b650eee9f979453be7cc23e58aa144f098393e3
Alex <alex@jintronix.com>
2024-05-22 08:55:21 -0400

    withered eruption aoe

--- src/backend/enemyAbilities/s4/bh.ts
+++ src/backend/enemyAbilities/s4/bh.ts
@@ -4,7 +4,10 @@ const gashFrenzy = getEnemySpell(378020, { cooldown: 60 })
 
 const earthBolt = getEnemySpell(378155, { avoidable: true })
 
-const witheredEruption = getEnemySpell(387264, { cooldown: 45 })
+const witheredEruption = getEnemySpell(387264, {
+  aoe: true,
+  cooldown: 45,
+})
 
 const stinkBreath = getEnemySpell(385186, {
   trashAbility: true,
