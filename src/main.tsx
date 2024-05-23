caffc3d59080d0c5b7120eb2c0c74f57e1972b0d
Alex <alex@jintronix.com>
2024-05-23 09:31:07 -0400

    change gtag

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import './globals.css'
-import TagManager from 'react-gtm-module'
 import { App } from './App'
 // import { StrictMode } from 'react'
-
-TagManager.initialize({ gtmId: 'G-N6CRB9B3CL' })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <StrictMode>
