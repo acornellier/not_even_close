441e41feca60fd65cce5fcfcd7dcd521e245416d
Alex <alex@jintronix.com>
2024-05-25 00:19:23 -0400

    update default stats

--- src/components/Characters/defaultCharacters.ts
+++ src/components/Characters/defaultCharacters.ts
@@ -1,5 +1,5 @@
 import type { Character } from '../../backend/characters.ts'
-import type { ClassSpec} from '../../backend/classes.ts';
+import type { ClassSpec } from '../../backend/classes.ts'
 import { defaultAbilities } from '../../backend/classes.ts'
 
 const defaultClassSpec: ClassSpec = { class: 'Monk', spec: 'Mistweaver' }
@@ -7,11 +7,11 @@ const defaultClassSpec: ClassSpec = { class: 'Monk', spec: 'Mistweaver' }
 export const defaultCharacter: Character = {
   classSpec: defaultClassSpec,
   stats: {
-    stamina: 41_000,
-    versatilityRaw: 1_000,
+    stamina: 65_000,
+    versatilityRaw: 3_000,
     avoidanceRaw: 325,
-    armor: 20_000,
-    mainStat: 10_000,
+    armor: 5_000,
+    mainStat: 20_000,
   },
   abilities: defaultAbilities(defaultClassSpec),
   externals: [],
