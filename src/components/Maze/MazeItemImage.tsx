c8af6b8916646f657b9b4375dd2aa8adbfb80e03
Alex Cornellier <alex@jintronix.com>
2024-07-11 15:48:27 -0400

    fog

---
+++ src/components/Maze/MazeItemImage.tsx
@@ -0,0 +1,26 @@
+﻿import type { MazeItem } from './mazeUtil.ts'
+
+const imageSize = 150
+
+export function MazeItemImage({ item }: { item: MazeItem }) {
+  const code = item.map((v) => (v ? 1 : 0)).join('')
+  return (
+    <div
+      className="relative cursor-pointer group maze-image"
+      style={{ height: imageSize, width: imageSize }}
+    >
+      <img
+        className="blur-lg grayscale-[0.5] group-hover:grayscale-0"
+        alt={code}
+        src={`/maze/${code}.png`}
+      />
+      <img alt={code} src={`/maze/${code}.png`} />
+      <div
+        className="group-hover:hidden"
+        style={{
+          backgroundImage: "url('/maze/fog.avif')",
+        }}
+      />
+    </div>
+  )
+}
