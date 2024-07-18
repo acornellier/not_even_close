27b563996ccbbdcea1d75a651e004718e19dadd7
Alex <alex@jintronix.com>
2024-07-17 21:36:09 -0400

    more ak spells

--- src/backend/enemyAbilities/tww_s1/ak.ts
+++ src/backend/enemyAbilities/tww_s1/ak.ts
@@ -1,7 +1,26 @@
 import { getEnemySpell } from '../grimoire.ts'
 
+const alertingShrill = getEnemySpell(438495, (spell) => ({
+  damage: spell.damage * 7,
+  periodic: true,
+}))
+
+const cosmicSingularity = getEnemySpell(432117, {
+  avoidable: true,
+})
+
+const callOfTheBrood = getEnemySpell(438879, (spell) => ({
+  damage: spell.damage * 3,
+  trashAbility: true,
+}))
+
 const massiveSlam = getEnemySpell(434252, {
   trashAbility: true,
 })
 
-export const akAbilities = [massiveSlam]
+export const akAbilities = [
+  alertingShrill,
+  cosmicSingularity,
+  callOfTheBrood,
+  massiveSlam,
+]
