788efb3eb1fda17596a3625935db82bcad9a6a1c
Alex <alex@jintronix.com>
2024-08-19 09:59:47 -0400

    remove dark orb

--- src/backend/enemyAbilities/tww_s1/db.ts
+++ src/backend/enemyAbilities/tww_s1/db.ts
@@ -1,9 +1,7 @@
 import { getEnemySpell } from '../grimoire.ts'
 
-const darkOrb = getEnemySpell(426826)
-
 const erosiveSpray = getEnemySpell(448888)
 
 const spinneretsStrands = getEnemySpell(434093)
 
-export const dbAbilities = [darkOrb, erosiveSpray, spinneretsStrands]
+export const dbAbilities = [erosiveSpray, spinneretsStrands]
