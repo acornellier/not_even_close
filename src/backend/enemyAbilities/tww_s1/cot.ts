3a86c1434dbd21d39b5b105be58a48cd7671d758
Alex <alex@jintronix.com>
2024-08-22 08:57:59 -0400

    more cot spells

--- src/backend/enemyAbilities/tww_s1/cot.ts
+++ src/backend/enemyAbilities/tww_s1/cot.ts
@@ -1,19 +1,42 @@
 import { getEnemySpell } from '../grimoire.ts'
 
-const iceSickles = getEnemySpell(440238)
+const vociferousIndoctrination = getEnemySpell(434832, (spell) => ({
+  damage: 5 * spell.damage,
+  periodic: true,
+  cooldown: 30,
+}))
 
 const webBolt = getEnemySpell(443427, {
   trashAbility: true,
   avoidable: true,
 })
 
+const darkPulse = getEnemySpell(437533, (spell) => ({
+  damage: 7 * spell.damage,
+  periodic: true,
+  cooldown: 70,
+}))
+
 const umbrealWeaveTrash = getEnemySpell(446718, {
   name: 'Umbral Weave (trash)',
   trashAbility: true,
 })
 
-const umbralWeave = getEnemySpell(439324, {
-  name: 'Umbral Weave (boss)',
+const tremorSlam = getEnemySpell(437700, {
+  effectIndex: 1,
+  cooldown: [40, 60],
 })
 
-export const cotAbilities = [iceSickles, webBolt, umbrealWeaveTrash, umbralWeave]
+const umbralWeave = getEnemySpell(439324, {
+  name: 'Umbral Weave (boss)',
+  cooldown: [40, 60],
+})
+
+export const cotAbilities = [
+  vociferousIndoctrination,
+  webBolt,
+  darkPulse,
+  umbrealWeaveTrash,
+  tremorSlam,
+  umbralWeave,
+]
