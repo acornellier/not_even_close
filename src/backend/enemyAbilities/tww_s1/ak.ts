309ffc0b4db7469ed1010724ed128df18ce0c557
Alex <alex@jintronix.com>
2024-08-22 09:12:59 -0400

    ak + gb cooldowns

--- src/backend/enemyAbilities/tww_s1/ak.ts
+++ src/backend/enemyAbilities/tww_s1/ak.ts
@@ -1,10 +1,5 @@
 import { getEnemySpell } from '../grimoire.ts'
 
-const alertingShrill = getEnemySpell(438495, (spell) => ({
-  damage: spell.damage * 7,
-  periodic: true,
-}))
-
 const cosmicSingularity = getEnemySpell(432117, {
   avoidable: true,
 })
@@ -12,14 +7,30 @@ const cosmicSingularity = getEnemySpell(432117, {
 const callOfTheBrood = getEnemySpell(438879, (spell) => ({
   damage: spell.damage * 3,
   trashAbility: true,
+  cooldown: 30,
+  periodic: true,
 }))
 
-const massiveSlam = getEnemySpell(434252, {
+const alertingShrill = getEnemySpell(438495, (spell) => ({
+  damage: spell.damage * 7,
+  cooldown: 40,
+  periodic: true,
+}))
+
+const gossamerOnslaught = getEnemySpell(438960, (spell) => ({
+  damage: spell.damage * 6,
+  cooldown: 40,
+  periodic: true,
+}))
+
+const massiveSlam = getEnemySpell(465012, {
   trashAbility: true,
+  cooldown: 25,
 })
 
 export const akAbilities = [
   alertingShrill,
+  gossamerOnslaught,
   cosmicSingularity,
   callOfTheBrood,
   massiveSlam,
--- src/backend/enemyAbilities/tww_s1/gb.ts
+++ src/backend/enemyAbilities/tww_s1/gb.ts
@@ -2,11 +2,13 @@ import { getEnemySpell } from '../grimoire.ts'
 
 const commandingRoar = getEnemySpell(448847, {
   ignoresArmor: true,
+  cooldown: 25,
 })
 
 const umbralWind = getEnemySpell(451939, {
   trashAbility: true,
   los: true,
+  cooldown: 23,
 })
 
 const earthBolt = getEnemySpell(451261, {
@@ -16,13 +18,17 @@ const earthBolt = getEnemySpell(451261, {
 
 const massTremor = getEnemySpell(451871, {
   trashAbility: true,
+  avoidable: true,
 })
 
 const moltenWake = getEnemySpell(451965, {
   trashAbility: true,
+  cooldown: 20,
 })
 
-const twilightBuffet = getEnemySpell(456751)
+const twilightBuffet = getEnemySpell(456751, {
+  cooldown: 35,
+})
 
 export const gbAbilities = [
   commandingRoar,
