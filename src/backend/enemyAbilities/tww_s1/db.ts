0263a57670c824a75fd22d0e0539980a2b304a26
Alex <alex@jintronix.com>
2024-08-23 20:36:08 -0400

    shorten name

--- src/backend/enemyAbilities/tww_s1/db.ts
+++ src/backend/enemyAbilities/tww_s1/db.ts
@@ -1,13 +1,13 @@
 import { getEnemySpell } from '../grimoire.ts'
 
 const burstingCocoon = getEnemySpell(451104, (spell) => ({
-  name: 'Bursting Cocoon (1 tick + hit)',
+  name: 'Bursting Cocoon',
   damage: (4 / 10) * spell.damage + spell.damage,
   aoeMultiplier: 0.714,
   trashAbility: true,
   cooldown: 20,
   notes:
-    'The final hit is always preceded immediately by a tick of damage. The dot is ST but the final hit is AoE.',
+    '1 tick + last hit. The final hit is always preceded immediately by a tick of damage. The dot is ST but the final hit is AoE.',
 }))
 
 const obsidianBeam = getEnemySpell(453310, {
