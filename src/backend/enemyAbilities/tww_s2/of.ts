b1422c9ca64660a0598dc02f59e09fe4ca092767
Alex <alex@jintronix.com>
2025-01-23 12:04:27 -0500

    of

--- src/backend/enemyAbilities/tww_s2/of.ts
+++ src/backend/enemyAbilities/tww_s2/of.ts
@@ -1,3 +1,39 @@
-import type { EnemyAbility } from '../enemies.ts'
+import { getEnemySpell } from '../grimoire.ts'
 
-export const ofAbilities: EnemyAbility[] = []
+const quickShot = getEnemySpell(460602)
+
+const deflagration = getEnemySpell(460814)
+
+const wallop = getEnemySpell(459799, {
+  tankOnly: true,
+})
+
+const sludgeClawsPhys = getEnemySpell(469479, (spell) => ({
+  name: `${spell.name} phys`,
+  tankOnly: true,
+  notes:
+    'This is only the physical damage part of the spell, you will need to check this and its pair separately',
+}))
+
+const sludgeClawsMagic = getEnemySpell(469480, (spell) => ({
+  name: `${spell.name} magic`,
+  tankOnly: true,
+  notes:
+    'This is only the magic damage part of the spell, you will need to check this and its pair separately',
+}))
+
+const gigazap = getEnemySpell(468815)
+
+const thunderPunch = getEnemySpell(466189, {
+  tankOnly: true,
+})
+
+export const ofAbilities = [
+  quickShot,
+  deflagration,
+  wallop,
+  sludgeClawsPhys,
+  sludgeClawsMagic,
+  gigazap,
+  thunderPunch,
+]
