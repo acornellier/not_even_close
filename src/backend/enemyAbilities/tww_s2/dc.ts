dfe93412e50ae48d7c8021c4b7aa4755bb0a3852
Alex <alex@jintronix.com>
2025-01-25 12:54:05 -0500

    dc

--- src/backend/enemyAbilities/tww_s2/dc.ts
+++ src/backend/enemyAbilities/tww_s2/dc.ts
@@ -1,3 +1,23 @@
-import type { EnemyAbility } from '../enemies.ts'
+import { bossSpell, trashSpell } from '../grimoire.ts'
 
-export const dcAbilities: EnemyAbility[] = []
+const overpoweringRoar = trashSpell(428066, {
+  ignoresArmor: true,
+})
+
+const throwRock = trashSpell(428089)
+
+const dousingBreath = bossSpell(425394)
+
+const massiveStomp = trashSpell(1218117, {
+  outrange: 50,
+})
+
+const shadowblast = bossSpell(427011)
+
+export const dcAbilities = [
+  overpoweringRoar,
+  throwRock,
+  dousingBreath,
+  massiveStomp,
+  shadowblast,
+]
