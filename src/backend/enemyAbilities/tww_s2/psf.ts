e97f09aeab5ffe617b942a30235a0bcbd17e6ef9
Alex <alex@jintronix.com>
2025-01-30 09:17:17 -0500

    psf

--- src/backend/enemyAbilities/tww_s2/psf.ts
+++ src/backend/enemyAbilities/tww_s2/psf.ts
@@ -1,3 +1,39 @@
-import type { EnemyAbility } from '../enemies.ts'
+import { bossSpell, trashSpell } from '../grimoire.ts'
 
-export const psfAbilities: EnemyAbility[] = []
+const impale = trashSpell(427621)
+
+const thunderclap = trashSpell(448492)
+
+const shieldSlam = trashSpell(448485, {
+  tankOnly: true,
+})
+
+const lungingStrike = trashSpell(424426)
+
+const heatWave = trashSpell(427897)
+
+const divineJudgment = trashSpell(448515, { tankOnly: true })
+
+const pierceArmor = bossSpell(424414, { tankOnly: true })
+
+const sacredToll = trashSpell(448791)
+
+const fireball = trashSpell(427469, { avoidable: true })
+
+const catigatorsShield = bossSpell(446649)
+
+const holySmite = trashSpell(427357, { avoidable: true })
+
+export const psfAbilities = [
+  impale,
+  thunderclap,
+  shieldSlam,
+  lungingStrike,
+  heatWave,
+  divineJudgment,
+  pierceArmor,
+  sacredToll,
+  fireball,
+  catigatorsShield,
+  holySmite,
+]
