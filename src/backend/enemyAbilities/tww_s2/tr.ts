99de782080f385f8c9447b91c0b626fb8e730505
Alex <alex@jintronix.com>
2025-04-07 10:44:16 -0400

    arcing void

--- src/backend/enemyAbilities/tww_s2/tr.ts
+++ src/backend/enemyAbilities/tww_s2/tr.ts
@@ -2,6 +2,8 @@ import { bossSpell, trashSpell } from '../grimoire.ts'
 
 const lightningBolt = trashSpell(430109, { avoidable: true })
 
+const arcingVoid = trashSpell(430805)
+
 const crashReality = bossSpell(425113)
 
-export const trAbilities = [lightningBolt, crashReality]
+export const trAbilities = [lightningBolt, arcingVoid, crashReality]
