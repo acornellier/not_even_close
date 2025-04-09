455c26251543e203cbd251f9feeb24dbd9263516
Alex <alex@jintronix.com>
2025-04-09 17:24:53 -0400

    crashing thunder

--- src/backend/enemyAbilities/tww_s2/tr.ts
+++ src/backend/enemyAbilities/tww_s2/tr.ts
@@ -2,8 +2,10 @@ import { bossSpell, trashSpell } from '../grimoire.ts'
 
 const lightningBolt = trashSpell(430109, { avoidable: true })
 
+const crashingThunder = bossSpell(1214324)
+
 const arcingVoid = trashSpell(430805)
 
 const crashReality = bossSpell(425113)
 
-export const trAbilities = [lightningBolt, arcingVoid, crashReality]
+export const trAbilities = [lightningBolt, crashingThunder, arcingVoid, crashReality]
