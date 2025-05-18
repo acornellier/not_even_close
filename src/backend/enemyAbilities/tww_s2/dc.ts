4cb62ca7b63853eb88e11b2066ecab4985c1c388
Alex <alex@jintronix.com>
2025-05-18 09:05:46 -0400

    quenching blast, eternal darkness

--- src/backend/enemyAbilities/tww_s2/dc.ts
+++ src/backend/enemyAbilities/tww_s2/dc.ts
@@ -6,6 +6,8 @@ const overpoweringRoar = trashSpell(428066, {
 
 const throwRock = trashSpell(428089)
 
+const quenchingBlast = trashSpell(430171)
+
 const dousingBreath = bossSpell(425394)
 
 const massiveStomp = trashSpell(1218117, {
@@ -14,10 +16,17 @@ const massiveStomp = trashSpell(1218117, {
 
 const shadowblast = bossSpell(427011)
 
+const eternalDarkness = bossSpell(428276, (spell) => ({
+  damage: spell.damage * 4,
+  periodic: true,
+}))
+
 export const dcAbilities = [
   overpoweringRoar,
   throwRock,
+  quenchingBlast,
   dousingBreath,
   massiveStomp,
   shadowblast,
+  eternalDarkness,
 ]
