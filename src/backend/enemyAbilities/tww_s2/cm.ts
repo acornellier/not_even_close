3389adaa6487e7a17f20f98758d3504333b88e1d
Alex <alex@jintronix.com>
2025-05-27 08:46:32 -0400

    tenderize ignores armor

--- src/backend/enemyAbilities/tww_s2/cm.ts
+++ src/backend/enemyAbilities/tww_s2/cm.ts
@@ -2,7 +2,9 @@ import { bossSpell, trashSpell } from '../grimoire.ts'
 
 const throwChair = trashSpell(434758)
 
-const tenderize = trashSpell(463206)
+const tenderize = trashSpell(463206, {
+  ignoresArmor: true,
+})
 
 const recklessDelivery = trashSpell(448935, {
   ignoresArmor: true,
