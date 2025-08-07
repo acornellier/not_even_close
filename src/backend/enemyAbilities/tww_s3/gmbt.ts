38ec642e2dfc5d55b723b2c447acbc48df0453ba
Alex <alex@jintronix.com>
2025-08-07 10:43:14 -0400

    gmbt

--- src/backend/enemyAbilities/tww_s3/gmbt.ts
+++ src/backend/enemyAbilities/tww_s3/gmbt.ts
@@ -1,5 +1,23 @@
-﻿import { bossSpell } from '../grimoire.ts'
+﻿import { bossSpell, trashSpell } from '../grimoire.ts'
 
 const purifyingBurst = bossSpell(353312)
 
-export const gmbtAbilities = [purifyingBurst]
+const waterbolt = trashSpell(355225, {
+  avoidable: true,
+})
+
+const tidalStomp = trashSpell(355429)
+
+const brackishBolt = trashSpell(356843, {
+  avoidable: true,
+})
+
+const hyperlightJolt = bossSpell(350885)
+
+export const gmbtAbilities = [
+  tidalStomp,
+  waterbolt,
+  purifyingBurst,
+  brackishBolt,
+  hyperlightJolt,
+]
