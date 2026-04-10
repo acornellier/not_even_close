92b3e209022a5d20105f6fb0ce15937ef94b3096
Alex <alex@cornellier.com>
2026-04-09 21:18:34 -0400

    add a bunch of AA abilities

--- src/backend/enemyAbilities/midnight/aa.ts
+++ src/backend/enemyAbilities/midnight/aa.ts
@@ -1,19 +1,36 @@
 import { bossSpell, trashSpell } from '../grimoire.ts'
 
-const arcaneExpulsion = bossSpell(385958, {
-  avoidable: true,
-})
+const burstForth = bossSpell(388923)
 
 const expelIntruders = trashSpell(377912, {
   avoidable: true,
 })
 
-const powerVacuum = bossSpell(388822, {
+const ragingScreech = trashSpell(1276632, {
+  ignoresArmor: true,
+})
+
+const deafeningScreech = bossSpell(377009)
+
+const surge = trashSpell(388862, {
   avoidable: true,
 })
 
+const arcaneFissure = bossSpell(388537)
+
+const arcaneBolt = trashSpell(1279627, {
+  avoidable: true,
+})
+
+const arcaneSmash = trashSpell(1270356)
+
 export const aaAbilities = [
-  arcaneExpulsion,
+  burstForth,
   expelIntruders,
-  powerVacuum,
+  ragingScreech,
+  deafeningScreech,
+  surge,
+  arcaneFissure,
+  arcaneBolt,
+  arcaneSmash,
 ]
