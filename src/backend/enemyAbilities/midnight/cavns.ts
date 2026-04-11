a8b8de46bc3b1add1ceaeac643874ac4c50d1849
Alex <alex@cornellier.com>
2026-04-11 08:59:59 -0400

    more spells

--- src/backend/enemyAbilities/midnight/cavns.ts
+++ src/backend/enemyAbilities/midnight/cavns.ts
@@ -6,12 +6,12 @@ const flankingSpear = bossSpell(1266485, {
   tankOnly: true,
 })
 
-const vilePotatoad = trashSpell(1257164, {
-  avoidable: true,
-})
+const frostNova = trashSpell(1271623)
 
-export const cavnsAbilities = [
-  deafeningRoar,
-  flankingSpear,
-  vilePotatoad,
-]
+const lingeringDread = bossSpell(1251813, (spell) => ({
+  name: `${spell.name} x2`,
+  damage: spell.damage * 2,
+  notes: 'Damage taken when 2 ghosts collide',
+}))
+
+export const cavnsAbilities = [deafeningRoar, flankingSpear, frostNova, lingeringDread]
